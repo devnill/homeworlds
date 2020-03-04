257c9b18896da52e21311f63b13e358f3ad208b8
Dan Singer <devnill@gmail.com>
2020-03-03 21:36:00 -0700

    forgot a file

---
+++ src/strings.js
@@ -0,0 +1,11 @@
+
+const error = {
+  invalidSystem: 'invalid system',
+  invalidShip: 'invalid ship',
+  catastrophyFailed: 'Cannot catastrophy there. Is it overpopulated?'
+};
+
+
+module.exports = {
+  error
+}
