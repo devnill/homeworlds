5fa4b9d1a9c6aea2529a6f5921526051e9c3fc8a
Dan Singer <devnill@gmail.com>
2020-03-16 23:17:30 -0600

    updated deps

--- test/actions/attack.js
+++ test/actions/attack.js
@@ -8,6 +8,7 @@ module.exports = function attackSpecs() {
   it('can only take equal or smaller ships', function () {
     const mock = deepfreeze(mocks.valid);
     const result = attack(mock.state, mock.action);
+    assert.isNull(result.err);
     expect(result.state).to.deep.equal(mock.result);
   });
   it('cannot take larger ships');
