697c913a51e53517d6f4aaf6421cfc48cebfbab7
Dan Singer <devnill@gmail.com>
2020-03-17 00:29:14 -0600

    cleaned up some cruft

--- src/actions/chooseHomeworld.js
+++ src/actions/chooseHomeworld.js
@@ -25,54 +25,6 @@ function chooseHomeworld(state, args) {
     bank: updatedBank,
     board: [...state.board, homeworld]
   });
-  /*
-  const updatedBank = {
-    red: [...state.bank.red],
-    blue: [...state.bank.blue],
-    green: [...state.bank.green],
-    yellow: [...state.bank.yellow]
-  };
-
-  const validationError = standardValidation(state, args);
-  if (validationError) {
-    return actionFailure(state, validationError);
-  }
-
-  const requiredPieces = countPieces([...args.stars, ...args.ships]);
-
-  for (const color in updatedBank) {
-    for (let size = 0; size < updatedBank[color].length; size++) {
-      updatedBank[color][size] -= requiredPieces[color][size];
-      if (updatedBank[color][size] < 0) {
-        return actionFailure(state, 'insufficent resources in bank to perform action');
-      }
-    }
-  }
-
-  const updatedBoard = [...state.board, {
-    name: 'player1',
-    star: [{
-      size: 3,
-      color: 'blue'
-    }, {
-      size: 2,
-      color: 'yellow'
-    }],
-    ships: [{
-      size: 3,
-      color: 'green',
-      owner: 'player1'
-    }]
-  }];
-
-  const updatedHistory = [...state.history, Object.assign({ action: 'chooseHomeworld', args })];
-
-  return actionSuccess(Object.assign({}, state, {
-    board: updatedBoard,
-    bank: updatedBank,
-    //history: updatedHistory
-  }));
-  */
 }
 
 module.exports = chooseHomeworld;
