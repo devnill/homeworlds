06ad466e57e9d37cec54dc02a14439b60a6a0535
Rachel Fish <itsrachelfish@gmail.com>
2020-03-17 20:42:46 -0600

    Added invalid size movement test

--- test/actions/move.js
+++ test/actions/move.js
@@ -5,12 +5,20 @@ const { expect, assert } = require('chai');
 const deepfreeze = require('deepfreeze');
 
 module.exports = function moveSpecs() {
-  it('cannot move between systems sharing a common sized star', function () {
-    const mock = deepfreeze(mocks.valid);
-    const result = move(mock.state, mock.action);
-    expect(result.state.board).to.deep.equal(mock.result.board);
+  it('cannot move between systems sharing a common sized star', function() {
+      const mock = deepfreeze(mocks.invalidSize);
+      const result = move(mock.state, mock.action);
+      assert.isNotNull(result.err);
+      expect(result.state.bank).to.deep.equal(mock.result.bank);
+      expect(result.state.board).to.deep.equal(mock.result.board);
   });
-  it('removes ship from first system and adds to the destination', );
+
+  it('removes ship from first system and adds to the destination', function() {
+      const mock = deepfreeze(mocks.valid);
+      const result = move(mock.state, mock.action);
+      expect(result.state.board).to.deep.equal(mock.result.board);
+  });
+
   it('takes a piece from the bank if the system is new');
   it('cannot move to a new system if the piece is not in the bank');
 };
--- test/mocks/move.js
+++ test/mocks/move.js
@@ -99,6 +99,91 @@ const valid = {
   }
 };
 
+const invalidSize = {
+  action: {
+    player: 'player1',
+    from: { id: 2 },
+    to: { id: 1 },
+    ship: { id: 3 } //moves ship 3 from system 1 to system 2
+  },
+  state: {
+    'bank': {
+      'red': [3, 3, 3],
+      'yellow': [2, 3, 3],
+      'green': [3, 3, 3],
+      'blue': [2, 3, 3]
+    },
+    'board': [{
+      id: 1,
+      name: 'Andoria',
+      stars: [{ color: 'blue', size: 2 }],
+      ships: [
+        { id: 5, color: 'yellow', size: 1, owner: 'player2' }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Sol',
+      stars: [{ color: 'yellow', size: 2 }],
+      ships: [
+        { id: 4, color: 'yellow', size: 1, owner: 'player2' },
+        { id: 3, color: 'red', size: 1, owner: 'player1' }
+      ]
+    }],
+    'players': ['player1', 'player2'],
+    'activePlayer': 0,
+    'turn': null,
+    'history': [/*{
+      action: 'move',
+      args: {
+        player: 'player1',
+        from: {id: 2},
+        to: {id: 1},
+        ship: {id: 3}
+      },
+      systems: []
+    }*/]
+  },
+  result: {
+    'bank': {
+      'red': [3, 3, 3],
+      'yellow': [2, 3, 3],
+      'green': [3, 3, 3],
+      'blue': [2, 3, 3]
+    },
+    'board': [{
+      id: 1,
+      name: 'Andoria',
+      stars: [{ color: 'blue', size: 2 }],
+      ships: [
+        { id: 5, color: 'yellow', size: 1, owner: 'player2' }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Sol',
+      stars: [{ color: 'yellow', size: 2 }],
+      ships: [
+        { id: 4, color: 'yellow', size: 1, owner: 'player2' },
+        { id: 3, color: 'red', size: 1, owner: 'player1' }
+      ]
+    }],
+    'players': ['player1', 'player2'],
+    'activePlayer': 0,
+    'turn': null,
+    'history': [/*{
+      action: 'move',
+      args: {
+        player: 'player1',
+        from: {id: 2},
+        to: {id: 1},
+        ship: {id: 3}
+      },
+      systems: []
+    }*/]
+  }
+};
+
 const systemLost = {
   action: {
     player: 'player1',
@@ -157,4 +242,4 @@ const systemLost = {
   }
 };
 
-module.exports = { valid, systemLost };
+module.exports = { valid, invalidSize, systemLost };
